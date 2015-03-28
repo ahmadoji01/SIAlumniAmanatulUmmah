747879a43999c58024dca75770d935d6fef95d98
DanielHenry <daniel.henry.39@gmail.com>
2015-03-28 22:37:36 +0700

    update controller pilihalumni

--- CodeIgniter/application/controllers/pilihalumni.php
+++ CodeIgniter/application/controllers/pilihalumni.php
@@ -6,65 +6,76 @@
 		{
 			parent::__construct();
 		}
-		public function caridataalumni()
+		public function profil()
 		{
 			$this->load->library('session');
 			$username = $this->session->userdata('username');
-			$this->load->model('Alumni');
-			$nama = $this->input->post('inputNama');
-			$tahunlulus = $this->session->userdata('tahunlulus');
-			$this->Alumni->setName($nama);
-			$this->Alumni->setTahunLulus($tahunlulus);
-			$data['NamaLengkap'] = $this->session->userdata('namalengkap');
-			if($nama != '')
-			{	
-				$data['query'] = $this->Alumni->searchMhs(9);
-			}
-			$data['nama'] = $nama;
-			$data['status'] = 0;
 			if ($username==false)
 			{
 				$this->load->helper('url');
 				redirect('home','location');
 			}
-			else if ($nama != '' && $data['query']->num_rows() > 0)
+			else if ($username[0]=='A')
 			{
-				$data['status'] = 1;
-				$this->load->view('view_carialumni', $data);
+				$this->load->helper('url');
+				redirect('home','location');
 			}
 			else
 			{
-				$data['status'] = 2;
-				$this->load->view('view_carialumni', $data);
+				$notifikasi;
+			
+				$this->load->model('Alumni');
+				$this->Alumni->setUsername($username);
+				$query = $this->Alumni->getAllFromDatabase();
+				if ($query->num_rows()>0)
+				{
+					foreach ($query->result() as $row)
+					{
+						//bukan pesan
+						$notifikasi['NamaLengkap'] = $row->NamaLengkap;
+						$notifikasi['Lembaga'] = $row->Lembaga;
+						$notifikasi['Cabang'] = $row->Cabang;
+						$notifikasi['AlamatSekarang'] = $row->AlamatSekarang;
+						$notifikasi['NoHP'] = $row->NoHP;
+						$notifikasi['Email'] = $row->Email;
+						$notifikasi['Facebook'] = $row->Facebook;
+						$notifikasi['Twitter'] = $row->Twitter;
+						$notifikasi['Blog'] = $row->Blog;
+						
+						$notifikasi['LinkFoto'] = $row->LinkFoto;
+						
+						$notifikasi['TempatKuliah'] = $row->TempatKuliah;
+						$notifikasi['Kesibukan'] = $row->Kesibukan;
+						$notifikasi['TempatKerja'] = $row->TempatKerja;
+						$notifikasi['Hobi'] = $row->Hobi;
+						$notifikasi['Cita_Cita'] = $row->Cita_Cita; 
+						$notifikasi['Motto'] = $row->Motto;
+						
+						$notifikasi['Prestasi'] = $row->Prestasi;
+						
+						$notifikasi['NamaOrtu'] = $row->NamaOrtu;
+						$notifikasi['PendidikanOrtu'] = $row->PendidikanOrtu;
+						$notifikasi['PekerjaanOrtu'] = $row->PekerjaanOrtu;
+						$notifikasi['KontakOrtu'] = $row->KontakOrtu;
+					}
+				}
+				$this->load->view('view_profil', $notifikasi);
 			}
 		}
-		/*public function uploadfoto()
+		public function tentang()
 		{
-			$this->load->library('session');
-			$username = $this->session->userdata('username');
-			$data['NamaLengkap'] = $this->session->userdata('namalengkap');
-			$data['status'] = 0;
-			$this->load->model('Alumni');
-			$this->Alumni->setUsername($username);
-			$data['link'] = $this->Alumni->getLinkFoto();
-			if ($username==false)
-			{
-				$this->load->helper('url');
-				redirect('home','location');
-			}
-			else
-			{
-				$this->load->view('view_uploadfoto', $data);
-			}
-		}*/
+		
+		}
+		public function ubahdatapribadi()
+		{
+		
+		}
 		public function uploadfoto()
 		{
 			$this->load->library('session');
-			$this->load->model('Alumni');
 			$username = $this->session->userdata('username');
 			$data['NamaLengkap'] = $this->session->userdata('namalengkap');
-			$this->Alumni->setUsername($username);
-			$data['link'] = $this->Alumni->getLinkFoto();
+			
 			// Check if image file is a actual image or fake image
 			if(isset($_POST["submit"])) 
 			{
@@ -75,7 +86,6 @@
 				else
 				{
 					$target_dir = realpath(__DIR__) . '/../../assets/profpic/';
-					$linkfoto = '../../assets/profpic/'. basename($_FILES["fileToUpload"]["name"]);
 					$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 					$uploadOk = 1;
 					$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
@@ -114,9 +124,6 @@
 						if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 							//echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 							$data['status'] = 1;
-							$this->Alumni->setUsername($username);
-							$this->Alumni->setLinkFoto($linkfoto);
-							$this->Alumni->updateFoto();
 						} else {
 							//echo "Sorry, there was an error uploading your file.";
 							$data['status'] = 2;
@@ -145,6 +152,38 @@
 				{
 					$this->load->view('view_uploadfoto', $data);
 				}
+			}
+		}
+		public function caridataalumni()
+		{
+			$this->load->library('session');
+			$username = $this->session->userdata('username');
+			$this->load->model('Alumni');
+			$nama = $this->input->post('inputNama');
+			$tahunlulus = $this->session->userdata('tahunlulus');
+			$this->Alumni->setName($nama);
+			$this->Alumni->setTahunLulus($tahunlulus);
+			$data['NamaLengkap'] = $this->session->userdata('namalengkap');
+			if($nama != '')
+			{	
+				$data['query'] = $this->Alumni->searchMhs(9);
+			}
+			$data['nama'] = $nama;
+			$data['status'] = 0;
+			if ($username==false)
+			{
+				$this->load->helper('url');
+				redirect('home','location');
+			}
+			else if ($nama != '' && $data['query']->num_rows() > 0)
+			{
+				$data['status'] = 1;
+				$this->load->view('view_carialumni', $data);
+			}
+			else
+			{
+				$data['status'] = 2;
+				$this->load->view('view_carialumni', $data);
 			}
 		}
 	}
